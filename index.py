e57665fbeb03184797880bc0c94d07bac3e2bc72
William Pepple <williampepple1@gmail.com>
2025-05-07 21:52:36 +0100

    update

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assessment_project.settings')
 
-application = get_asgi_application()
+application = get_wsgi_application()
 
 # Vercel requires this
 handler = application 
