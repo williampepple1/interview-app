fb74a19f4d22562602fe4e9ed5abd1d5d8b81b87
William Pepple <williampepple1@gmail.com>
2025-05-07 22:10:13 +0100

    update

--- assessment_project/settings.py
+++ assessment_project/settings.py
@@ -125,13 +125,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'static/css'),
-    os.path.join(BASE_DIR, 'static/js'),
-    os.path.join(BASE_DIR, 'static/images'),
 ]
 
 # Whitenoise configuration
