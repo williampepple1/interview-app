f3598562ce571f96081738da95323ca21851a6ca
William Pepple <williampepple1@gmail.com>
2025-05-07 21:55:39 +0100

    update

--- index.py
+++ index.py
@@ -5,5 +5,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assessment_project.settings')
 
 application = get_wsgi_application()
 
+def handler(event, context):
+    return application(event, context)
+
 # Vercel requires this
 handler = application 
